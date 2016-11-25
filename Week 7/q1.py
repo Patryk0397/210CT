11604eda0317ff86d9a06bc78abffb450f8fddf5
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2016-11-25 19:01:41 +0000

    deleted a w7q1 copy

--- Week 7/q1.py
+++ Week 7/q1.py
@@ -1,34 +1,76 @@
-class Graph:
-    def __init__(self):        
-        self.vertices = []              #initialise the class with a vertices array
-        self.edges = []                 #and edges array
-    def addEdge(self,a,b):
-        if a and b in self.vertices:    #check if a and b are in the vertices list before adding edges
-            edge = [a,b]                #if they are add the 2 nodes from the edge to a single array
-            self.edges.append(edge)     #append that array to the edges array
-        else:                           #if a or b are not vertices in the graph program stops and does nothing
+class graph:
+
+    def __init__(self):
+        self.dictionary = {}
+
+    def addVertex(self,vertex):
+        if vertex not in self.dictionary:
+            self.dictionary[vertex] = []
+        else:
             pass
-            
-            
+        
 
-    def addVertex(self,n):              #function adds a vertex to the vertices list
-        self.vertices.append(n)
+    def addEdge(self,vertex,edge):
+        self.dictionary[vertex].append(edge)
+        self.dictionary[edge].append(vertex)
 
+    def printDict(self):
+        for key in self.dictionary:
+            print(key, ':', self.dictionary[key])
 
-if __name__ == "__main__":
-    g = Graph()
+    def DFS(self, vertex):
+        
+        self.visited = []
+        self.stack= []
+
+        self.stack.append(vertex)
+
+        while self.stack != []:
+            u = self.stack.pop()
+            if u not in self.visited:
+                self.visited.append(u)
+                for edge in self.dictionary[u]:
+                    self.stack.append(edge)
+        print(self.visited)
+        return(self.visited)
+
+    def BFS(self,vertex):
+
+        self.q = []
+        self.visited = []
+
+        self.q.insert(0, vertex)
+
+        while self.q != []:
+            u = self.q.pop()
+            if u not in self.visited:
+                self.visited.append(u)
+                for edge in self.dictionary[u]:
+                    self.q.insert(0,edge)
+        print (self.visited)
+       
+        
+
+if __name__ == '__main__':
+    
+    g = graph()
+
+## 7 : 6                9
+## 6 :      7    8      9  
+## 8 : 6                9
+## 9 : 6    7    8
+    
     g.addVertex(7)
-    g.addVertex(1)
-    g.addVertex(3)
-    g.addVertex(5)
+    g.addVertex(6)
+    g.addVertex(8)
     g.addVertex(9)
-    g.addEdge(1,7)
-    g.addEdge(3,9)
-    g.addEdge(5,9)
-    print(g.vertices, g.edges)
-    
+    g.addEdge(6,7)
+    g.addEdge(6,8)
+    g.addEdge(6,9)
+    g.addEdge(7,9)
+    g.addEdge(9,8)
+    g.printDict()
+    g.DFS(7)
+    g.BFS(7)
 
-    
-    
-    
-    
+        
